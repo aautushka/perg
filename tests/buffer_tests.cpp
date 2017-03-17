3774b3a57d22db990849f2b713375cef15106638
aautushka <avtushko@gmail.com>
2017-03-17 17:43:02 +0300

    added memory buffer

--- tests/buffer_tests.cpp
+++ tests/buffer_tests.cpp
@@ -17,7 +17,6 @@ TEST_F(buffer_tests, adds_string)
 {
 	buf.copy("hello");
 	EXPECT_EQ("hello", buf.convert<std::string>());
-
 }
 
 TEST_F(buffer_tests, adds_string_byte_by_byte)
@@ -29,4 +28,18 @@ TEST_F(buffer_tests, adds_string_byte_by_byte)
 	buf.copy("o");
 	EXPECT_EQ("hello", buf.convert<std::string>());
 
+}
+
+TEST_F(buffer_tests, count_number_of_bytes_written)
+{
+	buf.copy("hello");
+	EXPECT_EQ(5, buf.size());
+}
+
+
+TEST_F(buffer_tests, allocates_a_little_more_than_needed_for_optimization_purposes)
+{
+	buf.copy("h");
+	buf.copy("e");
+	EXPECT_GT(buf.capacity(), buf.size());
 }
