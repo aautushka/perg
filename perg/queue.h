89f61f168740bba9274ac368a56f3f5e7ea35ca3
aautushka <marina.nenadovets@gmail.com>
2017-03-21 13:30:12 +0300

    add O(1) size to queue

--- perg/queue.h
+++ perg/queue.h
@@ -10,11 +10,17 @@ template <typename T>
 class queue
 {
 public:
+	queue()
+		: _size(0)
+	{
+	}
+
 	void push(T t)
 	{
 		std::lock_guard<std::mutex> lock(_mutex);
 
 		_data.push_back(t);
+		++_size;
 	}
 
 	T pop()
@@ -34,6 +40,7 @@ public:
 		if (!_data.empty())
 		{
 			t = _data.pop_front();
+			--_size;
 			return true;
 		}
 
@@ -46,11 +53,19 @@ public:
 		{
 			std::lock_guard<std::mutex> lock(_mutex);
 			out = std::move(_data);
+			_size = 0;
 		}
 		return std::move(out);
 	}
 
+	size_t size()
+	{
+		std::lock_guard<std::mutex> lock(_mutex);
+		return _size;
+	}
+
 private:
+	size_t _size;
 	perg::list<T> _data;
 	std::mutex _mutex;
 
--- tests/queue_tests.cpp
+++ tests/queue_tests.cpp
@@ -1,30 +1,75 @@
 #include <gtest/gtest.h>
 #include "perg/queue.h"
 
-struct perg_test : public ::testing::Test
+struct queue_test : public ::testing::Test
 {
 	perg::queue<int> q;
 };
 
-TEST_F(perg_test, enqueues_stuff)
+TEST_F(queue_test, enqueues_stuff)
 {
 	q.push(123);
 	EXPECT_EQ(123, q.pop());
 }
 
-TEST_F(perg_test, queue_is_of_fifo_type)
+TEST_F(queue_test, queue_is_of_fifo_type)
 {
 	q.push(123);
 	q.push(456);
 	EXPECT_EQ(123, q.pop());
 }
 
-TEST_F(perg_test, reads_last_item_on_the_queue)
+TEST_F(queue_test, reads_last_item_on_the_queue)
 {
 	q.push(123);
 	q.push(456);
 	q.pop();
 
 	EXPECT_EQ(456, q.pop());
+}
+
+TEST_F(queue_test, initially_the_queue_is_empty)
+{
+	EXPECT_EQ(0, q.size());
+}
+
+TEST_F(queue_test, queue_size_grows_as_new_item_comes_in)
+{
+	q.push(123);
+	EXPECT_EQ(1, q.size());
+}
+
+TEST_F(queue_test, queue_size_decreases_as_item_get_popped_out)
+{
+	q.push(123);
+	q.pop();
+	EXPECT_EQ(0, q.size());
+}
+
+TEST_F(queue_test, queue_size_remain_unchanged_if_unable_to_pop_an_item)
+{
+	int i = 0;
+	q.try_pop(i);
+	EXPECT_EQ(0, q.size());
+}
+
+
+TEST_F(queue_test, reads_everything_at_once)
+{
+	q.push(123);
+	q.push(456);
+
+	auto ll = q.pop_all();
+	EXPECT_EQ(123, ll.front());
+	EXPECT_EQ(456, ll.back());
+}
+
+TEST_F(queue_test, clears_the_queue)
+{
+	q.push(123);
+	q.push(456);
+	q.pop_all();
+
+	EXPECT_EQ(0, q.size());
 }
 
