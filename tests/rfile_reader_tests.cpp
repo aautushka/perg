4d2e3bfdb7aa573c4d53f93d4cdd0e74bf50bbcd
aautushka <avtushko@gmail.com>
2017-03-22 12:57:23 +0300

    added tests for reverse file reader

---
+++ tests/rfile_reader_tests.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include <stdlib.h>
+#include "perg/perg.h"
+#include "temp_file.h"
+
+struct rfile_reader_tests : public ::testing::Test
+{
+	perg::pipeline<perg::view> pipeline;
+	perg::sinks::collector<perg::view> sink;
+	perg::reverse_file_reader source;
+	tests::temp_file file;
+
+	using result = std::vector<perg::view>;
+
+	rfile_reader_tests()
+	{
+	}
+
+	void run_pipeline()
+	{
+		source.open(file.name());
+		pipeline.connect(source)(sink);
+		pipeline.wait();
+	}
+
+	~rfile_reader_tests()
+	{
+	}
+};
+
+TEST_F(rfile_reader_tests, reads_a_file_with_one_line_only)
+{
+	file.write_line("hello world");
+
+	run_pipeline();
+	
+	result expected = {"hello world\n"}; 
+	EXPECT_EQ(expected, sink.result());
+	
+}
+
+TEST_F(rfile_reader_tests, reads_multiple_lines_in_reverse_order)
+{
+	file.write_line("hello world");
+	file.write_line("how are you");
+
+	run_pipeline();
+	
+	result expected = {"how are you\n", "hello world\n"};
+	EXPECT_EQ(expected, sink.result());
+	
+}
+
+TEST_F(rfile_reader_tests, reads_whole_file_if_there_are_no_newlines)
+{
+	file.write("hello world");
+
+	run_pipeline();
+	
+	result expected = {"hello world"}; 
+	EXPECT_EQ(expected, sink.result());
+	
+}
