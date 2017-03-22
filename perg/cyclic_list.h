d73d6b906dec1308b347c1cd23ab58601f90b478
aautushka <avtushko@gmail.com>
2017-03-22 18:39:40 +0300

    added fixed-length list container

---
+++ perg/cyclic_list.h
@@ -0,0 +1,197 @@
+#pragma once
+
+namespace perg
+{
+template <typename T>
+constexpr bool power_of_two(T t)
+{
+	return t && (!(t & (t - 1)));
+}
+
+template <typename T, int N>
+class cyclic_list
+{
+public:
+	using self_type = cyclic_list<T, N>;
+
+	static_assert(power_of_two(N), "suboptimal performance");
+
+	bool empty() const
+	{
+		return count == 0;
+	}
+	
+	size_t capacity() const
+	{
+		return N;
+	}
+
+	bool full() const
+	{
+		return size() == capacity();	
+	}
+
+	size_t size() const
+	{
+		return count;
+	}
+
+	const T& front() const
+	{
+		assert(!empty());
+		return data[head];
+	}
+
+	const T& back() const
+	{
+		assert(!empty());
+		return data[prev(tail)];
+	}
+
+
+	T& front()
+	{
+		return data[head];
+	}
+
+	T& back()
+	{
+		return data[prev(tail)];
+	}
+
+	void clear()
+	{
+		count = head = tail = 0;
+	}
+
+	void push_back(T t)
+	{
+		assert(!full());
+
+		data[tail] = std::move(t);
+
+		move_tail_forward();
+	}
+
+	void push_front(T t)
+	{
+		assert(!full());
+
+		move_head_backward();
+
+		data[head] = std::move(t);
+	}
+
+	T pop_back()
+	{
+		assert(!empty());
+
+		move_tail_backward();
+
+		return std::move(data[tail]);
+	}
+
+	T pop_front()
+	{
+		assert(!empty());
+
+		T t  = std::move(data[head]);
+		move_head_forward();
+
+		return std::move(t);
+	}
+
+	class iterator
+	{
+	public:
+		using list_t = cyclic_list<T, N>;
+
+		iterator(){ }
+		iterator(list_t& list) : list(&list), cur(list.head), left(list.size()) { }
+
+		bool operator ==(const iterator& other) const 
+		{
+			return (list == other.list && cur == other.cur) 
+				|| (list == nullptr && other.left == 0) 
+				|| (other.list == nullptr && left == 0); 
+		}
+
+		bool operator !=(const iterator& other) const { return !(*this == other); }
+		T& operator *() { return list->data[cur]; } 
+		const T& operator *() const { return list->data[cur]; }
+		iterator& operator ++() { cur = list->next(cur); --left; } 
+		iterator operator ++(int) { iterator prev(*this); ++(*this); return prev; }
+		T* operator ->() { &**this; }
+		const T* operator ->() const { &**this; }
+
+	private:
+		list_t* list = nullptr;
+		int cur = 0;
+		int left = 0;
+	};
+
+	iterator begin() 
+	{
+		return iterator(*this);
+	}
+
+	iterator end()
+	{
+		return iterator();
+	}
+private:
+	void move_tail_forward()
+	{
+		count += 1;
+		move_forward(tail);
+	}
+
+	void move_head_backward()
+	{
+		count += 1;
+		move_backward(head);
+	}
+
+	void move_tail_backward()
+	{
+		count -= 1;
+		move_backward(tail);
+	}
+
+	void move_head_forward()
+	{
+		count -= 1;
+		move_forward(head);
+	}
+
+	void move_backward(int& ptr)
+	{
+		ptr = ((N - 1) + ptr) % N;
+	}
+
+	void move_forward(int& ptr)
+	{
+		ptr = (ptr + 1) % N;
+	}
+
+	int prev(int ptr)
+	{
+		move_backward(ptr);
+		return ptr;
+	}
+
+	int next(int ptr)
+	{
+		move_forward(ptr);
+		return ptr;
+	}
+
+	int head = 0;
+	int tail = 0;
+	int count = 0;
+	T data[N];
+};
+
+} // namespace perg
+
+
---
+++ tests/cyclic_list_tests.cpp
@@ -0,0 +1,168 @@
+#include <gtest/gtest.h>
+#include "perg/cyclic_list.h"
+
+struct cyclic_list_test : public ::testing::Test
+{
+	perg::cyclic_list<int, 4> list;
+};
+
+TEST_F(cyclic_list_test, list_is_empty_once_created)
+{
+	EXPECT_TRUE(list.empty());
+	EXPECT_EQ(0, list.size());
+	EXPECT_FALSE(list.full());
+}
+
+TEST_F(cyclic_list_test, list_is_no_longer_empty)
+{
+	list.push_back(123);
+	EXPECT_FALSE(list.empty());
+}
+
+TEST_F(cyclic_list_test, accesses_element_just_added_to_the_tail)
+{
+	list.push_back(123);
+	EXPECT_EQ(123, list.front());
+	EXPECT_EQ(123, list.back());
+}
+
+TEST_F(cyclic_list_test, accesses_element_just_added_to_the_head)
+{
+	list.push_front(123);
+	EXPECT_EQ(123, list.front());
+	EXPECT_EQ(123, list.back());
+}
+
+TEST_F(cyclic_list_test, accesses_head_and_tail_separately)
+{
+	list.push_back(123);
+	list.push_back(456);
+
+	EXPECT_EQ(123, list.front());
+	EXPECT_EQ(456, list.back());
+}
+
+TEST_F(cyclic_list_test, pushes_elements_to_the_head)
+{
+	list.push_front(123);
+	list.push_front(456);
+
+	EXPECT_EQ(456, list.front());
+	EXPECT_EQ(123, list.back());
+}
+
+TEST_F(cyclic_list_test, list_capacity_matches_the_template_argument)
+{
+	EXPECT_EQ(4, list.capacity());
+}
+
+TEST_F(cyclic_list_test, list_is_not_full_until_consumes_all_capacity)
+{
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+
+	EXPECT_FALSE(list.full());
+}
+
+TEST_F(cyclic_list_test, list_is_full)
+{
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(4);
+
+	EXPECT_EQ(4, list.size());
+	EXPECT_TRUE(list.full());
+}
+
+TEST_F(cyclic_list_test, reaches_full_capacity_by_growing_list_from_its_head)
+{
+	list.push_front(1);
+	list.push_front(2);
+	list.push_front(3);
+	list.push_front(4);
+
+	EXPECT_EQ(4, list.size());
+	EXPECT_TRUE(list.full());
+}
+
+
+TEST_F(cyclic_list_test, clears_list)
+{
+	list.push_back(1);
+	list.clear();
+
+	EXPECT_TRUE(list.empty());
+}
+
+TEST_F(cyclic_list_test, push_back_changes_list_size)
+{
+	list.push_back(1);
+
+	EXPECT_EQ(1, list.size());
+}
+
+TEST_F(cyclic_list_test, push_front_changes_list_size)
+{
+	list.push_front(1);
+
+	EXPECT_EQ(1, list.size());
+}
+
+TEST_F(cyclic_list_test, pop_front_changes_list_size)
+{
+	list.push_front(1);
+	list.pop_front();
+
+	EXPECT_EQ(0, list.size());
+}
+
+TEST_F(cyclic_list_test, pop_back_changes_list_size)
+{
+	list.push_front(1);
+	list.pop_back();
+
+	EXPECT_EQ(0, list.size());
+}
+
+TEST_F(cyclic_list_test, pop_back_returns_element_being_removed)
+{
+	list.push_back(1);
+	list.push_back(2);
+
+	EXPECT_EQ(2, list.pop_back());
+}
+
+TEST_F(cyclic_list_test, pop_front_returns_element_being_removed)
+{
+	list.push_back(1);
+	list.push_back(2);
+
+	EXPECT_EQ(1, list.pop_front());
+}
+
+TEST_F(cyclic_list_test, list_at_full_capacity_it_not_empty)
+{
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(4);
+
+	EXPECT_FALSE(list.empty());
+}
+
+TEST_F(cyclic_list_test, iterates_over_list)
+{
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(4);
+
+	std::vector<int> expected = {1, 2, 3, 4};
+	std::vector<int> actual;
+
+	for (int i : list ) actual.push_back(i);
+	EXPECT_EQ(expected, actual);
+
+}
